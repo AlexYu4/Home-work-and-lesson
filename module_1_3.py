bb2d3ef91141003cd4ea169e22cfd6ce8fd83a44
AlexYu4 <selendione@gmail.com>
2024-12-06 18:37:18 +0700

    Home work

--- module_1_3.py
+++ module_1_3.py
@@ -6,3 +6,11 @@ new_age = 32
 print(new_age,type(new_age))
 is_student = True
 print(is_student,type(is_student))
+# Определение переменных
+number_of_homeworks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_homework = hours_spent / number_of_homeworks
+
+# Вывод на экран
+print(f"Курс: {course_name}, всего задач: {number_of_homeworks}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_homework} часа.")
