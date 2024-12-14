c504b9f5e4791c1004e629e83bb1a625b64b3cd0
AlexYu4 <selendione@gmail.com>
2024-12-14 18:00:32 +0700

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "Alex"
 print(name,type(name))
 age = 31
 print(age,type(age))
-new_age = 32
+new_age = (age+1)
 print(new_age,type(new_age))
 is_student = True
 print(is_student,type(is_student))
